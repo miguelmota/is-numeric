7c046a4e2ce1686a3bd37ec74d82a7b0784bc253
Miguel Mota <miguelmota2@gmail.com>
2018-02-11 12:05:18 -0800

    more tests

--- test/is-numeric.js
+++ test/is-numeric.js
@@ -1,51 +1,56 @@
-var test = require('tape');
-var isNumeric = require('../is-numeric');
+var test = require('tape')
+var isNumeric = require('../is-numeric')
 
 test('isNumeric', function (t) {
-  'use strict';
+  'use strict'
 
-  t.plan(42);
+  t.plan(47)
 
-  t.equal(isNumeric(0), true);
-  t.equal(isNumeric(123), true);
-  t.equal(isNumeric(-123), true);
-  t.equal(isNumeric(+123), true);
-  t.equal(isNumeric('0'), true);
-  t.equal(isNumeric('123'), true);
-  t.equal(isNumeric('-123'), true);
-  t.equal(isNumeric('+123'), true);
-  t.equal(isNumeric('.123'), true);
-  t.equal(isNumeric('-.123'), true);
-  t.equal(isNumeric('0.123'), true);
-  t.equal(isNumeric('-0.123'), true);
-  t.equal(isNumeric('1e100'), true);
-  t.equal(isNumeric('1e-100'), true);
-  t.equal(isNumeric('-1e-100'), true);
-  t.equal(isNumeric(Infinity), true);
-  t.equal(isNumeric(-Infinity), true);
-  t.equal(isNumeric(new String('123')), true);
-  t.equal(isNumeric(new Number('123')), true);
-  t.equal(isNumeric('abc'), false);
-  t.equal(isNumeric('10%'), false);
-  t.equal(isNumeric('#10'), false);
-  t.equal(isNumeric('2^10'), false);
-  t.equal(isNumeric('2!'), false);
-  t.equal(isNumeric('(10)'), false);
-  t.equal(isNumeric('10px'), false);
-  t.equal(isNumeric('*'), false);
-  t.equal(isNumeric(''), false);
-  t.equal(isNumeric(' '), false);
-  t.equal(isNumeric(true), false);
-  t.equal(isNumeric(false), false);
-  t.equal(isNumeric([]), false);
-  t.equal(isNumeric(function(){}), false);
-  t.equal(isNumeric({}), false);
-  t.equal(isNumeric(undefined), false);
-  t.equal(isNumeric(null), false);
-  t.equal(isNumeric(NaN), false);
-  t.equal(isNumeric('  -3'), true);
-  t.equal(isNumeric('  -3', {trim: false}), false);
-  t.equal(isNumeric('3 ', {trim: false}), false);
-  t.equal(isNumeric('  3 ', {trim: false}), false);
-  t.equal(isNumeric(' 1e10 ', {trim: false}), false);
-});
+  t.equal(isNumeric(0), true)
+  t.equal(isNumeric(123), true)
+  t.equal(isNumeric(-123), true)
+  t.equal(isNumeric(+123), true)
+  t.equal(isNumeric('0'), true)
+  t.equal(isNumeric('123'), true)
+  t.equal(isNumeric('-123'), true)
+  t.equal(isNumeric('+123'), true)
+  t.equal(isNumeric('.123'), true)
+  t.equal(isNumeric('-.123'), true)
+  t.equal(isNumeric('0.123'), true)
+  t.equal(isNumeric('-0.123'), true)
+  t.equal(isNumeric('1e100'), true)
+  t.equal(isNumeric('1e-100'), true)
+  t.equal(isNumeric('-1e-100'), true)
+  t.equal(isNumeric(Infinity), true)
+  t.equal(isNumeric(-Infinity), true)
+  t.equal(isNumeric(new String('123')), true)
+  t.equal(isNumeric(new Number('123')), true)
+  t.equal(isNumeric('abc'), false)
+  t.equal(isNumeric('10%'), false)
+  t.equal(isNumeric('#10'), false)
+  t.equal(isNumeric('2^10'), false)
+  t.equal(isNumeric('2!'), false)
+  t.equal(isNumeric('(10)'), false)
+  t.equal(isNumeric('10px'), false)
+  t.equal(isNumeric('*'), false)
+  t.equal(isNumeric(''), false)
+  t.equal(isNumeric(' '), false)
+  t.equal(isNumeric(true), false)
+  t.equal(isNumeric(false), false)
+  t.equal(isNumeric([]), false)
+  t.equal(isNumeric(function(){}), false)
+  t.equal(isNumeric({}), false)
+  t.equal(isNumeric(undefined), false)
+  t.equal(isNumeric(null), false)
+  t.equal(isNumeric(NaN), false)
+  t.equal(isNumeric('  -3'), true)
+  t.equal(isNumeric('  -3', {trim: false}), false)
+  t.equal(isNumeric('3 ', {trim: false}), false)
+  t.equal(isNumeric('  3 ', {trim: false}), false)
+  t.equal(isNumeric(' 1e10 ', {trim: false}), false)
+  t.equal(isNumeric('1e10', {trim: false}), true)
+  t.equal(isNumeric('100', {trim: false}), true)
+  t.equal(isNumeric('1e10', {trim: true}), true)
+  t.equal(isNumeric('100', {trim: true}), true)
+  t.equal(isNumeric('  -3', {trim: true}), true)
+})
